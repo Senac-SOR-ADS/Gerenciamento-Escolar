939daddfca62cc1f1f9ce41ac5abdb231f915437
Riq <ruylivinginadream@duck.com>
2026-02-20 11:25:00 -0300

    run

--- App/run.py
+++ App/run.py
@@ -1,3 +1,16 @@
 from PyQt5.QtWidgets import QApplication
+from App.view.loginUi import LoginUI
+from App.view.homeUI import HomeUI
+from App.controller.loginController import LoginController, isLogged
 
 app = QApplication([])
+login = LoginUI()
+
+while not isLogged():
+    res = login.exec_()
+    if res:
+        tela = HomeUI()
+        app.exec_()
+    else:
+        break
+print('programa encerrado')
