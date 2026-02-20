0ebb42f6a8c3121f2634eba0b007f250998afc7b
Riq <ruylivinginadream@duck.com>
2026-02-20 11:55:05 -0300

    contol

--- App/controller/loginController.py
+++ App/controller/loginController.py
@@ -8,3 +8,15 @@ __currentUser = {
 
 def isLogged():
     return __currentUser['id']
+
+def __setCurrentUser(id):
+    __currentUser['id'] = id
+
+def validateLogin(email, senha):
+    if email == "usuario" and senha == "123":
+        __setCurrentUser(1)
+        return True
+    return False
+
+def logout():
+    __setCurrentUser(None)
