d5abd2bbcce18bcdeadcf2eb78b3f9391358ae06
Caiquef80 <caiquecampos.767@gmail.com>
2026-03-04 11:59:36 -0300

    adicionando metodos crud para usuario

--- App/controller/loginController.py
+++ App/controller/loginController.py
@@ -1,5 +1,6 @@
 from App.config.database import Database
 from App.model.userModel import Usuario
+from App.utils.criptografia import Criptografia
  
 __currentUser = {
     "id": None,
@@ -20,7 +21,8 @@ def __setCurrentUser(id):
 def validateLogin(email , password):
     user = Usuario.login(email)
     user.showInfo()
-    if user.id and user.email == email and user.senha == password:
+    result_senha = Criptografia.compararSenha(password , user.senha)
+    if user.id and user.email == email and result_senha:
         __setCurrentUser(user.id)
         return True
     #if not result:
--- App/model/userModel.py
+++ App/model/userModel.py
@@ -2,24 +2,53 @@ from App.config.database import Database
 
 class Usuario:
     id = None
-    nome = ""
+    name = ""
     email = ""
-    senha = ""
-    tipo = ""
-    ativo = False
+    password = ""
+    type = ""
+    active = False
 
-    def __init__(self , id=None , nome="" , email="" , senha="" , tipo="" , ativo=False):
+    def __init__(self , id=None , name="" , email="" , password="" , type="" , active=False):
         self.id = id
-        self.nome = nome
+        self.name = name
         self.email = email
-        self.senha = senha
-        self.tipo = tipo 
-        self.ativo = bool(ativo)
+        self.password = password
+        self.type = type 
+        self.active = bool(active)
+
+    @classmethod
+    def createUser(cls , name , email , password , type):
+        pass
+
+    @classmethod
+    def updateUser(cls , id):
+        pass
+    @classmethod
+    def deleteUser(cls, id):
+        pass
+    @classmethod
+    def findByUser(cls, id):
+        pass
+    @classmethod
+    def findAllUser(cls):
+        try:
+            DB = Database()
+            sql = "SELECT * FROM usuarios"
+            result = DB.fetchAll(sql)
+            return result
+        except Exception as e:
+            print(f'Erro ao buscar todos os usuarios {e}')
+            raise RuntimeError
+            
+
+    @classmethod
+    def findUserActive(cls):
+        pass
 
     @classmethod
     def login(cls , email):
         DB = Database()
-        sql = "SELECT * FROM usuarios WHERE email = %s"
+        sql = "SELECT * FROM usuarios WHERE email = %s AND ativo = 1"
         params = (email,)
         result = DB.fetchOne(sql, params)
         if not result: return Usuario()
@@ -30,9 +59,13 @@ class Usuario:
     def showInfo(self):
         print(f"""
         ID : {self.id}
-        Nome : {self.nome}
+        Nome : {self.name}
         Email: {self.email}
-        Senha: {self.senha}
-        Tipo: {self.tipo}
-        Ativo: {self.ativo}
+        Senha: {self.password}
+        Tipo: {self.type}
+        Ativo: {self.active}
          """)
+
+if __name__ == "__main__":
+    todosUsuarios = Usuario.findAllUser()
+    print(todosUsuarios)
