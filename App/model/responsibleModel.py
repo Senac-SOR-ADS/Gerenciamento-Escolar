61989d6230982a2b7541f64f0219b09a0ce8d148
iScZ7 <igor.salatiel.carniato@gmail.com>
2026-03-06 11:53:08 -0300

    Create - Responsaveis

--- App/model/responsibleModel.py
+++ App/model/responsibleModel.py
@@ -13,13 +13,25 @@ class Responsavel:
         self.cpf = cpf
         self.legal_guardian = bool(legal_guardian)
 
-    def create(self):
-        #Cadastra o responsável
-        pass
+    @classmethod
+    def create(cls, name, cpf):
+            DB = Database()
+            sql = "INSERT INTO responsaveis(nome, cpf) VALUES (%s, %s)"
+            valores = (name, cpf)
+            novo_id = DB.insert(sql, valores)
+            print('Cadastrado com sucesso')
+            return novo_id
+
+
+        
 
     def update(self):
         #Atualiza o responsável
         pass
+
+    @classmethod
+    def _getObjectList(cls, lista):
+        return [cls(*user) for user in lista]
 
     @classmethod
     def getAll(cls):
@@ -27,7 +39,7 @@ class Responsavel:
             DB = Database()
             sql = "SELECT * FROM responsaveis"
             result = DB.fetchall(sql)
-            return result
+            return cls._getObjectList(result)
         except Exception as e:
             print(f'Erro ao buscar os responsaveis{e}')
             raise RuntimeError
@@ -40,13 +52,19 @@ class Responsavel:
     def searchLegalGuardian(self):
         #Busca responsáveis legais
         pass
-
-    def delete(self):
-        # "Desativa" um responsável
-        pass
+    
+    @classmethod
+    #def delete(cls, id):
+        #DB = Database()
+        #sql = "DELETE FROM responsaveis WHERE id= %s"
+        #d = (id,)
+        #excluse = DB.execute(sql, d)
+        #return excluse
 
 if __name__ == "__main__":
     todosResponsaveis = Responsavel.getAll()
-    print(todosResponsaveis) 
+    print(todosResponsaveis)
+    Responsavel.create("Yuri Alberto", "12345678903")
+    #Responsavel.delete(4)
 
 
