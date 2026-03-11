efd46484d832dad91b126050b74b9136fb4e3fde
Gabriel <morenobiel2008@gmail.com>
2026-03-11 09:28:40 -0300

    fix: Correção da main
    Remoção da  OS na main

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 if __name__ == "__main__":
     from App import run
-
-    import os
-    from dotenv import load_dotenv
-    
-    # Ele carrega as variaveis no arquivo .env
-    load_dotenv(override=True)
-    print(os.environ)
