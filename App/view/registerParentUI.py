50772f81483c863900f1f7b63b44ffcdc548235a
psfrancieli <francielisilva2011@gmail.com>
2026-03-11 11:52:56 -0300

    feat: criação das telas individuais
    telas abrindo individualmente e com layout adicionado do qt. falta layout em algumas telas

---
+++ App/view/registerParentUI.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.uic import loadUi
+
+class RegisterParent(QDialog):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        loadUi("App/view/ui/registerParent.ui", self)
+        self.show()
+
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication([])
+    login = RegisterParent()
+    app.exec_()
