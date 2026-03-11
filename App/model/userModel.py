aa6266a87ffa3871793beebb292e536c96c61eea
Caiquef80 <caiquecampos.767@gmail.com>
2026-03-11 10:01:21 -0300

    feat terminando a classe usuario
    foi implementado os metodos cruds que estavam faltando

--- App/model/userModel.py
+++ App/model/userModel.py
@@ -1,12 +1,7 @@
 from App.config.database import Database
 
 class Usuario:
-    id = None
-    name = ""
-    email = ""
-    password = ""
-    type = ""
-    active = False
+    
 
     def __init__(self , id=None , name="" , email="" , password="" , type="" , active=False):
         self.id = id
@@ -18,24 +13,63 @@ class Usuario:
 
     @classmethod
     def createUser(cls , name , email , password , type):
-        pass
+        try:
+            DB = Database()
+            sql = "INSERT INTO usuarios (nome_user , email , senha , tipo_user) VALUES(%s , %s , %s , %s)"
+            params = (name , email , password , type)
+            result = DB.execute(sql  , params)
+            return result
+        except Exception as e:
+            print(f'Erro na criação do usuario {e}')
+            raise RuntimeError
 
     @classmethod
-    def updateUser(cls , id):
-        pass
+    def updateUser(cls ,id , name , email , password):
+        try:
+            DB = Database()
+            sql = "UPDATE usuarios SET nome_user = %s , email = %s , senha = %s WHERE id = %s"
+            params = (name , email , password , id)
+            result = DB.execute(sql , params)
+            return result
+        except Exception as e:
+            print(f'Não foi possivel atualizar os dados do usuario {e}')
+            raise RuntimeError
+        
     @classmethod
     def deleteUser(cls, id):
-        pass
+        try:
+            DB = Database()
+            sql = "UPDATE usuarios SET ativo = 0 WHERE id = %s"
+            params = (id , )
+            result = DB.execute(sql, params)
+            return result
+        except Exception as e:
+            print(f'Erro ao tentar desativar o usuario {e}')
+            raise RuntimeError
+        
     @classmethod
-    def findByUser(cls, id):
-        pass
+    def findById(cls, id):
+        try:
+            DB = Database()
+            sql = "SELECT * FROM usuarios WHERE id = %s"
+            params = (id,)
+            result = DB.fetchOne(sql , params)
+            if not result:
+                return None
+            return cls(*result)
+            
+        except Exception as e:
+            print(f'Erro ao encontrar usuario por id {e}')
+            raise RuntimeError
+
     @classmethod
-    def findAllUser(cls):
+    def findAll(cls):
         try:
             DB = Database()
             sql = "SELECT * FROM usuarios"
             result = DB.fetchAll(sql)
-            return result
+            usuarios = [cls(*row) for row in result]
+            return usuarios
         except Exception as e:
             print(f'Erro ao buscar todos os usuarios {e}')
             raise RuntimeError
@@ -43,7 +77,15 @@ class Usuario:
 
     @classmethod
     def findUserActive(cls):
-        pass
+        try:
+            DB = Database()
+            sql = "SELECT * FROM usuarios WHERE ativo = 1"
+            result = DB.fetchAll(sql)
+            usuarios = [cls(*row) for row in result]
+            return usuarios
+        except Exception as e:
+            print(f'Erro ao listar usuarios ativos {e}')
+            raise RuntimeError
 
     @classmethod
     def login(cls , email):
@@ -67,5 +109,5 @@ class Usuario:
          """)
 
 if __name__ == "__main__":
-    todosUsuarios = Usuario.findAllUser()
+    todosUsuarios = Usuario.findAll()
     print(todosUsuarios)
