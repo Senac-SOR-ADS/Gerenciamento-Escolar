56953aa9e4ef68107999b7bc4a13da154653307f
psfrancieli <francielisilva2011@gmail.com>
2026-03-18 11:56:35 -0300

    feat: tela cadastro conectada com o banco
    precisa alterar colocando try/except

--- App/view/registerEmployeeUI.py
+++ App/view/registerEmployeeUI.py
@@ -1,15 +1,64 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QComboBox
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.uic import loadUi
+from App.controller.userController import UserController
 
 class RegisterEmployeeUI(QDialog):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         loadUi("App/view/ui/registerEmployee.ui", self)
         self.show()
+        self.funcionario: QComboBox
+
+    def clearText(self):
+        self.nome.clear()
+        self.login.clear()
+        self.senha.clear()
+        self.senha_2.clear()
+        self.funcionario.setCurrentIndex(0)
+
+    def validarCampos(self):
+        nome = self.nome.text()
+        login = self.login.text()
+        senha = self.senha.text()
+        senha_2 = self.senha_2.text()
+        funcionario = self.funcionario.currentText()
+
+
+        if not nome and not login and not senha:
+            print(f'Preencha os campos!')
+            return
+
+        if senha != senha_2:
+            print(f'As senhas são diferentes!')
+            return
+        return {
+            "name": nome,
+            "email": login,
+            "password": senha,
+            "type": funcionario
+        }
+
+    @pyqtSlot()
+    def on_btn_concluir_clicked(self):
+        user = self.validarCampos()
+
+        if user:
+            print(user)
+            UserController.createUser(user)
+            self.clearText()
+
 
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
     app = QApplication([])
     login = RegisterEmployeeUI()
     app.exec_()
+
+# if user:
+#     print(user)
+#     try:
+#         UserController.createUser(user)
+#         self.clearText()
+#     except Exception as e:
+#         print(f"Erro: \n{e}")
