b12563df069f16a44aa248f469e280c9c1dd2e70
Riq <ruylivinginadream@duck.com>
2026-03-25 11:40:21 -0300

    report e report card

--- App/view/newReportUI.py
+++ App/view/newReportUI.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QLineEdit, QPushButton
 from PyQt5.QtCore import pyqtSlot, QDate
 from PyQt5.uic import loadUi
+from App.controller.reportController import ReportController as rc
+
 
 class NewReportUI(QDialog):
     def __init__(self, **kwargs):
@@ -10,9 +12,9 @@ class NewReportUI(QDialog):
         
         self.dataOcorrencia.setCalendarPopup(True)
         self.dataOcorrencia.setDate(QDate.currentDate())
-        self.btnSaveReport.clicked.connect(self.getText)
+        self.btnSaveReport.clicked.connect(lambda: self.sendReport(self.getInfo()))
     
-    def getText(self):
+    def getInfo(self):
         info = []
         desc = self.descricaoOcorrencia.text()
         resp = self.responsavelCombo.currentText()
@@ -20,9 +22,16 @@ class NewReportUI(QDialog):
         date = self.dataOcorrencia.text()
         
         info = desc, resp, name, date
-        print(info)
+        print(info[0])
         
-        return info   
+        return info
+    
+    def sendReport(self, info):
+        try:
+            rc.create(info[0], 1, 1)
+        except Exception as e:
+            print(f"Erro ao criar o relatório! {e}")
+    
     
 
 if __name__ == "__main__":
