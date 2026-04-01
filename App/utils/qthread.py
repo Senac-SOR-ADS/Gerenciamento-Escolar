78f9337825a315ffb8ac75a2f1bf9f9f1ba556cc
psfrancieli <francielisilva2011@gmail.com>
2026-04-01 11:49:07 -0300

    feat: adicionando cep/endereco
    cep trazendo o endereço da api, falta conectar o registro do responsável com o banco

--- App/utils/qthread.py
+++ App/utils/qthread.py
@@ -3,10 +3,12 @@ from PyQt5.QtCore import pyqtSignal, QThread
 class Trabalhador(QThread):
     finalizado = pyqtSignal(object)
 
-    def __init__(self, callback):
+    def __init__(self, callback, *args, **kwargs):
         super().__init__()
         self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
 
     def run(self):
-        resultado = self.callback()
-        self.finalizado.emit(resultado) 
+        resultado = self.callback(*self.args, **self.kwargs)
+        self.finalizado.emit(resultado)
--- App/view/registerParentUI.py
+++ App/view/registerParentUI.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QDialog
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog, QLineEdit
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from PyQt5.uic import loadUi
 from App.utils.qthread import Trabalhador
 from App.controller.AddressController import AddressController
@@ -9,18 +9,46 @@ class RegisterParentUI(QDialog):
         super().__init__(**kwargs)
         loadUi("App/view/ui/registerParent.ui", self)
         self.show()
+        self.cep.editingFinished.connect(self.buscarCEP)
+        
+    def buscarCEP(self):
+        cep = self.cep.text()
+        self.validarCEP =Trabalhador(AddressController.requestCep, cep=cep)
+        self.validarCEP.finalizado.connect(self.popularCEP)
+        self.validarCEP.finished.connect(self.validarCEP.deleteLater)
+        self.validarCEP.start()
+
+    def popularCEP(self, dadosCEP):
+        self.cidade.setText(dadosCEP.get("city"))
+        self.bairro.setText(dadosCEP.get("neighborhood"))
+        self.rua.setText(dadosCEP.get("street"))
+
+    def validadeAll(self):
+        nome = self.nome.text()
+        telefone = self.telefone.text()
+        cep = self.cep.text()
+        resp_legal = self.resp_legal.text()
+
+        return {
+            "name": nome,
+            "phone": telefone,
+            "resp_legal": resp_legal,
+            "cep": cep
+        }
 
     @pyqtSlot()
     def on_btn_concluir_clicked(self):
-        self.cep =Trabalhador(AddressController.requestCep)
-        self.cep.finalizado.connect(self.create)
-        self.cep.finished.connect(self.cep.deleteLater)
-        self.cep.start()
+        resp = self.validadeAll()
+        print(resp)
 
-        self.btn_concluir.setEnabled(False)
+
+
 
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
     app = QApplication([])
     login = RegisterParentUI()
     app.exec_()
+
+
+    # 18053000
