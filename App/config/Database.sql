6db6c4a080604f164704fa0af689e59a3afbcb4b
Caiquef80 <caiquecampos.767@gmail.com>
2026-04-01 11:11:05 -0300

    feat Atualizando o database para usar o dados do banco atual

--- App/config/Database.sql
+++ App/config/Database.sql
@@ -1,76 +1,78 @@
-CREATE DATABASE IF NOT EXISTS gerenciamento_escolar
-USE gerenciamento_escolar
+CREATE DATABASE IF NOT EXISTS gerenciamento_escolar;
+USE gerenciamento_escolar;
 
 CREATE TABLE IF NOT EXISTS `salas` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `turmas` VARCHAR(30) NOT NULL UNIQUE,
-    `ativo` BOOLEAN,
-    `data` DATE NOT NULL
-);
+  `id` int(11) NOT NULL,
+  `turmas` varchar(30) NOT NULL,
+  `ativo` tinyint(1) DEFAULT 1,
+  `data` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `alunos` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `nome` VARCHAR(150) NOT NULL,
-    `nome_social` VARCHAR(150),
-    `CPF` VARCHAR(12) NOT NULL UNIQUE,
-    `data_nasc` DATE NOT NULL,
-    `RA` VARCHAR(15) NOT NULL UNIQUE,
-    `RM` VARCHAR(15) NOT NULL UNIQUE,
-    `Observacao` TEXT,
-    `status` BOOLEAN NOT NULL DEFAULT 1,
-    `data_registro` DATE NOT NULL DEFAULT (CURRENT_DATE)
-);
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `nome_social` varchar(150) DEFAULT NULL,
+  `CPF` varchar(12) NOT NULL,
+  `data_nasc` date NOT NULL,
+  `RA` varchar(15) NOT NULL,
+  `RM` varchar(15) NOT NULL,
+  `Observacao` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `data_registro` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `responsaveis` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `nome` VARCHAR(150) NOT NULL,
-    `CPF` VARCHAR(12) NOT NULL UNIQUE,
-    `responsavel_legal` BOOLEAN NOT NULL DEFAULT 0
-);
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `CPF` varchar(12) NOT NULL,
+  `responsavel_legal` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 CREATE TABLE IF NOT EXISTS `usuarios` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `nome_user` VARCHAR(150) NOT NULL,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `senha` VARCHAR(255) NOT NULL,
-    `tipo_user` ENUM('adm', 'secretaria', 'agente') NOT NULL,
-    `ativo` BOOLEAN NOT NULL DEFAULT 1
-);
+  `user_id` int(11) NOT NULL,
+  `nome_user` varchar(150) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_user` enum('adm','secretaria','agente') NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
  
 
 CREATE TABLE IF NOT EXISTS `sala_alunos` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `id_aluno` INTEGER NOT NULL,
-    `id_sala` INTEGER NOT NULL,
-    FOREIGN KEY (`id_aluno`) REFERENCES `alunos`(`id`),
-    FOREIGN KEY (`id_sala`) REFERENCES `salas`(`id`) 
-);
+  `id` int(11) NOT NULL,
+  `id_aluno` int(11) NOT NULL,
+  `id_sala` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `responsavel_aluno` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `aluno_id` INTEGER NOT NULL,
-    `responsavel_id` INTEGER NOT NULL,
-    FOREIGN KEY (`aluno_id`) REFERENCES `alunos`(`id`),
-    FOREIGN KEY (`responsavel_id`) REFERENCES `responsaveis`(`id`)
-);
+  `id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `responsavel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `enderecos` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `cidade` VARCHAR(100) NOT NULL,
-    `bairro` VARCHAR(100) NOT NULL,
-    `rua` VARCHAR(100) NOT NULL,
-    `complemento` VARCHAR(100),
-    `responsavel_id` INTEGER NOT NULL,
-    FOREIGN KEY (`responsavel_id`) REFERENCES `responsaveis`(`id`)
-);
+  `id` int(11) NOT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `rua` varchar(100) NOT NULL,
+  `numero` varchar(5) DEFAULT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `CEP` varchar(9) DEFAULT NULL,
+  `responsavel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `telefones` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `telefone` VARCHAR(18) NOT NULL,
-    `responsavel_id` INTEGER NOT NULL,
-    FOREIGN KEY (`responsavel_id`) REFERENCES `responsaveis`(`id`)
-);
+  `id` int(11) NOT NULL,
+  `telefone` varchar(18) NOT NULL,
+  `responsavel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `relatorios` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `data` DATE NOT NULL DEFAULT (CURRENT_DATE),
-    `descricao` TEXT NOT NULL,
-    `aluno_id` INTEGER NOT NULL,
-    `responsavel_id` INTEGER NOT NULL,
-    FOREIGN KEY (`aluno_id`) REFERENCES `alunos`(`id`),
-    FOREIGN KEY (`responsavel_id`) REFERENCES `responsaveis`(`id`)
-);
+  `id` int(11) NOT NULL,
+  `data` date NOT NULL DEFAULT curdate(),
+  `descricao` text NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `responsavel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
