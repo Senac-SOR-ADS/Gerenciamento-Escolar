2f9bd2eaf2f28aeecee9da7b6e5cad6ec3f656be
Richard <risilgamer@gmail.com>
2026-04-15 11:53:23 -0300

    atualizações

--- App/view/registerClassUI.py
+++ App/view/registerClassUI.py
@@ -1,15 +1,26 @@
 from PyQt5.QtWidgets import QDialog, QDateEdit
 from PyQt5.QtCore import QDate
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from PyQt5.uic import loadUi
 from App.controller.roomController import RoomController
 
 class RegisterClassUI(QDialog):
-    def __init__(self, **kwargs):
+
+    signalIdRoom = pyqtSignal(object)
+
+    def __init__(self, turmas, **kwargs):
         super().__init__(**kwargs)
         loadUi("App/view/ui/registerClass.ui", self)
+        self.room = turmas
+        self.turma.setText(self.room.turmas)
+        self.btn_confirmar.clicked.connect(self.mostrar_id)
         self.clearText()
         self.show()
+
+    def mostrar_id(self):
+        print(self.room.id)
+        self.signalIdRoom.emit(self.room.id)
+
 
     def clearText(self):
         self.turma.clear()
@@ -32,6 +43,8 @@ class RegisterClassUI(QDialog):
     def on_btn_confirmar_clicked(self):
         classe = self.validarCampos()
 
+        
+
         if classe:
             try:
                 RoomController.createRoom(*classe.values())
@@ -39,12 +52,12 @@ class RegisterClassUI(QDialog):
             except Exception as e:
                 print(f"Erro: \n{e}")
 
+   
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
     app = QApplication([])
-    login = RegisterClassUI()
+    login = RegisterClassUI(turmas=[0])
     app.exec_()
-
 
 
 
