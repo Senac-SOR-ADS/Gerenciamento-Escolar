bc6e883a69468f871aa52ac0a65ac76970200964
Gabriel <morenobiel2008@gmail.com>
2026-04-15 11:46:17 -0300

    feat: Está sendo criado o report com ids corretos

--- App/controller/parentController.py
+++ App/controller/parentController.py
@@ -41,13 +41,32 @@ class ParentController:
         except Exception as e:
             print(f'Erro ao tentar a atualização de usuario {e}')
             raise RuntimeError
-        
+    
+    @classmethod
+    def findParentForStudent(cls, studentID):
+        try:
+            studentID = ParentController.validateID(studentID)
+            lista = Parent.findParentForStudent(studentID)
+            return lista
+        except Exception as e:
+            raise e
+
+    
+    @classmethod
+    def validateID(cls, value):
+        if not isinstance(value, int): raise TypeError(f"ID incorreto")
+        if value <= 0: raise ValueError("Id invalido")
+        return value
     
 
 if __name__ == "__main__":
-    testParent = {
-        "name" : "cavalo",
-        "cpf" : "1234567233",
-        "address" : {"city" : "Sorocaba" , "neighborhood" : "Paineras" , "street" : "Vitor Gomes", "complement" : "Scrum-Master", "cep" : "1909192"}
-    }
-    ParentController.create(testParent)
+    # testParent = {
+    #     "name" : "cavalo",
+    #     "cpf" : "1234567233",
+    #     "address" : {"city" : "Sorocaba" , "neighborhood" : "Paineras" , "street" : "Vitor Gomes", "complement" : "Scrum-Master", "cep" : "1909192"}
+    # }
+    # ParentController.create(testParent)
+
+    a= ParentController.findParentForStudent(10)
+
+    print(a)
