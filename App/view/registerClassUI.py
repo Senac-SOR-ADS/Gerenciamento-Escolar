68c0b03f1d71a70c5dcebc727bb2996a0dae8c67
Richard <risilgamer@gmail.com>
2026-04-17 09:49:28 -0300

    Feat: Signal do registerClass

--- App/view/registerClassUI.py
+++ App/view/registerClassUI.py
@@ -1,6 +1,5 @@
-from PyQt5.QtWidgets import QDialog, QDateEdit
-from PyQt5.QtCore import QDate
-from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtCore import QDate, pyqtSlot, pyqtSignal
 from PyQt5.uic import loadUi
 from App.controller.roomController import RoomController
 
@@ -8,57 +7,49 @@ class RegisterClassUI(QDialog):
 
     signalIdRoom = pyqtSignal(object)
 
-    def __init__(self, turmas, **args, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, parent=None):
+        super().__init__(parent)
         loadUi("App/view/ui/registerClass.ui", self)
-        self.room = turmas
-        self.args = args
-        self.kwargs = kwargs
-        self.btn_confirmar.clicked.connect(self.mostrar_id)
+        self.signalIdRoom.connect(self.idRoom)
         self.clearText()
         self.show()
-
-    def mostrar_id(self):
-        resultado = self.room(*self.args, **self.kwargs)
-        self.signal_CEP.emit(resultado)
-
 
     def clearText(self):
         self.turma.clear()
         self.data.setDate(QDate.currentDate())
-        
+
     def validarCampos(self):
         turma = self.turma.text()
         data = self.data.date() #retorna um QDate
         data = data.toString("yyyy/MM/dd") #transforma em padrão EUA
 
-        if not turma and not data:
-            print(f'Preencha os campos!')
+        if not turma or not data:
+            print("Preencha todos os campos!")
+            return None
 
         return {
             "room": turma,
             "date": data
         }
-    
+        
+
     @pyqtSlot()
     def on_btn_confirmar_clicked(self):
         classe = self.validarCampos()
 
-        
-
         if classe:
             try:
-                RoomController.createRoom(*classe.values())
+                resultado = RoomController.createRoom(*classe.values())
+                self.signalIdRoom.emit(resultado) 
                 self.clearText()
             except Exception as e:
                 print(f"Erro: \n{e}")
 
-   
+    def idRoom (self, resultado):
+        print(f"Sinal capturado com sucesso! O resultado recebido foi: {resultado}")
+
+
 if __name__ == "__main__":
-    from PyQt5.QtWidgets import QApplication
     app = QApplication([])
     register = RegisterClassUI()
     app.exec_()
-
-
-
