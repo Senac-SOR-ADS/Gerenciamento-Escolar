de1d48cf5495d2344d80e0b07de7979d8b92f280
psfrancieli <francielisilva2011@gmail.com>
2026-04-17 11:05:07 -0300

    teste compilar

--- output/main/_internal/App/utils/loadFile.py
+++
@@ -1,8 +0,0 @@
-import sys
-from os import path
-
-def loadFile(file):
-    base_path = getattr(sys, "_MEIPASS", path.dirname(path.abspath(__file__)))
-    print(globals())
-    print(base_path, file)
-    return path.join(base_path, file)
