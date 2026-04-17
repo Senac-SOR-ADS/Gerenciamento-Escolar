5bb1cb278c58c906d4e5d6303046993d765d57cf
iScZ7 <igor.salatiel.carniato@gmail.com>
2026-04-17 08:25:44 -0300

    Feat - studentCardUI
    Modificado a variável "pessoa" para "person".

--- App/view/studentCardUI.py
+++ App/view/studentCardUI.py
@@ -11,8 +11,8 @@ class StudentCardUI(QWidget):
         loadUi("App/view/ui/studentCard.ui", self)
 
         self.student = student
-        self.pessoa = self.student.nome_social or self.student.nome
-        self.studentName.setText(self.pessoa)
+        self.person = self.student.nome_social or self.student.nome
+        self.studentName.setText(self.person)
         
         self.studentName.clicked.connect(self.openScreen)
         self.newReport.clicked.connect(self.openScreen)
@@ -35,7 +35,7 @@ class StudentCardUI(QWidget):
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
     from App.controller.studentController import Student
-    student = Student.findById(12)
+    student = Student.findById(13)
     app = QApplication([])
     login = StudentCardUI(student)
     login.show()
