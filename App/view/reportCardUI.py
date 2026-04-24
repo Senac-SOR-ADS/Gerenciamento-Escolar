7153a119678403c15ac11cdfe058fa6f470035d5
Richard <risilgamer@gmail.com>
2026-04-24 11:40:55 -0300

    Feat Report Screen

---
+++ App/view/reportCardUI.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.uic import loadUi
+
+class ReportCardUI(QDialog):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        loadUi("App/view/ui/reportCard.ui", self)
+        self.show()
+
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication([])
+    login = ReportCardUI()
+    app.exec_()
