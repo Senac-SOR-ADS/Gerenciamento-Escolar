429f61fa865c042e40025db8da8d7900f9f720fc
nickzin-art <nicolasbez2009.11@gmail.com>
2026-05-06 11:55:55 -0300

    commit meio caminho

--- App/view/reportUI.py
+++ App/view/reportUI.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.uic import loadUi
+from App.view.reportCardUI import ReportCardUI
 
 class ReportUI(QDialog):
     def __init__(self, studentId, **kwargs):
@@ -8,6 +9,26 @@ class ReportUI(QDialog):
         loadUi("App/view/ui/report.ui", self)
         self.studentId = studentId
         self.show()
+
+    def listReport(self, reports):
+        self.clearStackCards(self.scrollAreaWidgetContent)
+        
+        try:
+            for report in reports:
+                card = ReportCardUI(report)
+                self.addCardInStackReports(card)
+        except Exception as e:
+            pass
+
+    def addCardInStackReports(self, interface):
+        self.scrollAreaWidgetContent.layout().addWidget(interface)
+
+    def clearStackCards(self, scrollArea):
+        layout = scrollArea.layout()
+        for i in range(layout.count()):
+            layout.itemAt(i).widget().deleteLater()
+
+
 
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
